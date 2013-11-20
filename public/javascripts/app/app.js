96270695af3fabb70e3b00e4fd9bca39d9cd5c80
Peter Himmelreich <averygoodplanet@gmail.com>
2013-11-20 14:35:18 -0600

    Initial Commit

---
+++ public/javascripts/app/app.js
@@ -0,0 +1,22 @@
+/* global document, window, io */
+
+$(document).ready(initialize);
+
+var socket;
+
+function initialize(){
+  $(document).foundation();
+  initializeSocketIO();
+}
+
+function initializeSocketIO(){
+  var port = window.location.port ? window.location.port : '80';
+  var url = window.location.protocol + '//' + window.location.hostname + ':' + port + '/app';
+
+  socket = io.connect(url);
+  socket.on('connected', socketConnected);
+}
+
+function socketConnected(data){
+  console.log(data);
+}
